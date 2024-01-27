f1d7cce4548a78a1fb64ddaa7d6cb49f2767c6b3
NadaArafat <n.1762002@gmail.com>
2024-01-27 10:08:29 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tunes_player/views/tunes_view.dart';
+
+void main() {
+  runApp(const TunesApp());
+}
+
+class TunesApp extends StatelessWidget {
+  const TunesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TunesView(),
+    );
+  }
+}
---
+++ lib/views/tunes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TunesView extends StatelessWidget {
+  const TunesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
